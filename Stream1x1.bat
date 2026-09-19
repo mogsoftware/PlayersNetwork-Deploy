9bcd4eaff7566071a44398a15262b274f5e1feea
MWint <mwint@otoma.com.br>
2026-09-19 14:30:57 -0300

    Update FFmpeg streaming parameters: increase bitrate and maxrate for improved stream quality

--- Stream1x1.bat
+++ Stream1x1.bat
@@ -9,7 +9,7 @@ echo ====================================================
 :loop
 echo Starting Stream for %COMPUTERNAME% in 1x1 format 720p
 :: Place your exact FFmpeg command right below this line:
-ffmpeg -loglevel error -f gdigrab -framerate 24 -i desktop -c:v libx264 -preset ultrafast -profile:v high -pix_fmt yuv420p -an -g 48 -keyint_min 48 -sc_threshold 0 -b:v 500k -maxrate 1000k -bufsize 3000k -tune zerolatency -f mpegts "srt://10.10.0.5:8890?streamid=publish:%COMPUTERNAME%"
+ffmpeg -loglevel error -f gdigrab -framerate 24 -i desktop -c:v libx264 -preset ultrafast -profile:v high -pix_fmt yuv420p -an -g 48 -keyint_min 48 -sc_threshold 0 -b:v 1000k -maxrate 1500k -bufsize 3000k -s 800x450 -tune zerolatency -f mpegts "srt://10.10.0.5:8890?streamid=publish:%COMPUTERNAME%"
 echo FFmpeg crashed or stopped! Restarting in 2 seconds...
 timeout /t 2
 goto loop
