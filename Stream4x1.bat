286ad534e2d2791cd62a660c2915dcdb953607b8
MWint <mwint@otoma.com.br>
2026-09-23 15:13:41 -0300

    Update FFmpeg command: revert to lower resolution and bitrate settings for improved performance

--- Stream4x1.bat
+++ Stream4x1.bat
@@ -8,7 +8,7 @@ echo             Activating FFmpeg Stream
 echo ====================================================
 :loop
 echo Starting Wide Stream for %COMPUTERNAME% in 4x1 format 720p
-ffmpeg -loglevel error -f gdigrab -framerate 24 -i desktop -filter_complex "[0:v]crop=1920:540:0:0,scale=1280:360[top]; [0:v]crop=1920:540:0:540,scale=1280:360[bottom]; [top][bottom]hstack=inputs=2" -c:v libx264 -preset ultrafast -profile:v high -pix_fmt yuv420p -an -g 48 -keyint_min 48 -sc_threshold 0 -b:v 1500k -maxrate 1500k -bufsize 3000k -tune zerolatency -f mpegts "srt://10.10.0.5:8890?streamid=publish:%COMPUTERNAME%"
+ffmpeg -loglevel error -f gdigrab -framerate 24 -i desktop -filter_complex "[0:v]crop=1024:300:0:0,scale=682:200:flags=fast_bilinear[top]; [0:v]crop=1024:300:0:300,scale=682:200:flags=fast_bilinear[bottom]; [top][bottom]hstack=inputs=2" -c:v libx264 -preset ultrafast -profile:v baseline -pix_fmt yuv420p -an -g 48 -keyint_min 48 -sc_threshold 0 -b:v 1000k -maxrate 1000k -bufsize 2000k -f mpegts "srt://10.10.0.5:8890?streamid=publish:%COMPUTERNAME%"
 echo FFmpeg crashed or stopped! Restarting in 2 seconds...
 timeout /t 2
 goto loop
