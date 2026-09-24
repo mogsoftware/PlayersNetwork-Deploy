d5c3073d78af0d69dd4adf8499a52d0f8cc5fba3
MWint <mwint@otoma.com.br>
2026-09-24 19:07:42 -0300

    Update FFmpeg command in Stream1x1.bat and Stream4x1.bat for improved streaming quality and performance

--- Stream1x1.bat
+++ Stream1x1.bat
@@ -9,7 +9,7 @@ echo ====================================================
 :loop
 echo Starting Stream for %COMPUTERNAME% in 1x1 format 720p
 :: Place your exact FFmpeg command right below this line:
-ffmpeg -loglevel error -f gdigrab -framerate 24 -i desktop -c:v libx264 -preset ultrafast -profile:v main -pix_fmt yuv420p -an -g 48 -keyint_min 48 -sc_threshold 0 -b:v 2500k -maxrate 3500k -bufsize 7000k -f mpegts "srt://10.10.0.5:8890?streamid=publish:%COMPUTERNAME%"
+ffmpeg -loglevel error -f gdigrab -framerate 24 -i desktop -vf "scale=960:540:flags=fast_bilinear" -c:v libx264 -preset ultrafast -profile:v main -pix_fmt yuv420p -an -g 96 -keyint_min 96 -sc_threshold 0 -b:v 2500k -maxrate 3500k -bufsize 7000k -f mpegts "srt://10.10.0.5:8890?streamid=publish:%COMPUTERNAME%"
 echo FFmpeg crashed or stopped! Restarting in 2 seconds...
 timeout /t 2
 goto loop
--- Stream4x1.bat
+++ Stream4x1.bat
@@ -8,7 +8,7 @@ echo             Activating FFmpeg Stream
 echo ====================================================
 :loop
 echo Starting Wide Stream for %COMPUTERNAME% in 4x1 format 720p
-ffmpeg -loglevel error -f gdigrab -framerate 24 -i desktop -filter_complex "[0:v]crop=1024:300:0:0,scale=682:200:flags=fast_bilinear[top]; [0:v]crop=1024:300:0:300,scale=682:200:flags=fast_bilinear[bottom]; [top][bottom]hstack=inputs=2" -c:v libx264 -preset ultrafast -profile:v baseline -pix_fmt yuv420p -an -g 48 -keyint_min 48 -sc_threshold 0 -b:v 1000k -maxrate 1000k -bufsize 2000k -f mpegts "srt://10.10.0.5:8890?streamid=publish:%COMPUTERNAME%"
+ffmpeg -loglevel error -f gdigrab -framerate 24 -i desktop -filter_complex "[0:v]crop=1024:300:0:0,scale=682:200:flags=fast_bilinear[top]; [0:v]crop=1024:300:0:300,scale=682:200:flags=fast_bilinear[bottom]; [top][bottom]hstack=inputs=2" -c:v libx264 -preset ultrafast -profile:v main -pix_fmt yuv420p -an -g 96 -keyint_min 96 -sc_threshold 0 -b:v 2500k -maxrate 3500k -bufsize 7000k -f mpegts "srt://10.10.0.5:8890?streamid=publish:%COMPUTERNAME%"
 echo FFmpeg crashed or stopped! Restarting in 2 seconds...
 timeout /t 2
 goto loop
